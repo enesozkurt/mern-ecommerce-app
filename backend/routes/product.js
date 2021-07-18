47b4e46f92d0ed9e601fea10ee80b1f9520d46e5
enesokurt <enes.ozkurt4@gmail.com>
2021-07-18 18:12:56 +0300

    update, delete and get single product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router();
 
-const { getProducts, newProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
 
 router.route('/products').get(getProducts);
 
+router.route('/product/:id').get(getSingleProduct);
+
 router.route('/product/new').post(newProduct);
+
+router.route('/product/:id').put(updateProduct);
+
+router.route('/product/:id').delete(deleteProduct);
 
 module.exports = router;
